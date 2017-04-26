bcb76d93295f1ebe54e2d1018298b760346aa7d5
David Guy Brizan <dgrizan>
2017-04-26 15:04:33 -0700

    Added comments

--- person.py
+++ person.py
@@ -1,27 +1,52 @@
+'''
+This class encapsulates everything we know about a person: name, bio and spouse.
+We make the assumption that bio and spouse may be empty / missing.
+@author dbrizan
+'''
+
+
 class person:
 
     def __init__(self, name):
+        # This is the first function called when a person object is instantiated.
+        # * Input: name = a dict with value for key 'f' representing a first name
+        #                 ... and value for key 'last' representing a surname.
+        # * Output: none
+        # There are three fields: name (defaulted to an empty dict), biography
+        # (defaulted to an empty list) and spouse (defaulted to a None object).
         self.name = dict()
         self.biography = []
         self.spouse = None
         self.change_name(name)
 
     def add_to_bio(self, words):
+        # Mutator for biography: adds argument to the list of items in the biography.
+        # Input: words (string) to be added to biography
+        # Output: none
         self.biography.append(words)
 
     def get_biography(self):
+        # Accessor for biography.
+        # Input: none
+        # Output: the biography as a single string, entries separated by newline.
         bio = ''
         for bio_entry in self.biography:
             bio += '  + ' + bio_entry + '\n'
         return bio
 
     def change_name(self, name):
+        # Mutator for name.
+        # Input: name = dictionary with first name(s) keyed by 'f' and surname keyed by 'last'
+        # Output: none
         if 'f' in name:
             self.name['f'] = name['f']
         if 'last' in name:
             self.name['last'] = name['last']
 
     def get_name(self):
+        # Accessor for name.
+        # Input: none
+        # Output: the name as a single string.
         name = ''
         if 'f' in self.name:
             name += self.name['f']
@@ -32,12 +57,21 @@ class person:
         return name
 
     def change_spouse(self, spouse):
+        # Mutator for spouse.
+        # Input: spouse (person object)
+        # Output: none
         self.spouse = spouse
 
     def get_spouse(self):
+        # Accessor for spouse.
+        # Input: none
+        # Output: spouse (person).
         return self.spouse
 
     def to_string(self):
+        # Creates a string representation for the person object.
+        # Input: none
+        # Output: A string, formatted so that name, spouse and bio appear on different lines
         details = self.get_name() + '\n'
         if self.get_spouse() is not None:
             details += '* Married to: ' + self.get_spouse().get_name() + '\n'
--- test-person.py
+++ test-person.py
@@ -1,21 +1,28 @@
+'''
+I use the "person" class... and print using that.
+'''
 from person import person
 
 #
 # Main
 #
 if __name__ == '__main__':
+    # Create a person named "Guido".
     guido_name = {'f': 'Guido', 'last': 'van Rossum'}
     guido = person(guido_name)
 
+    # Add to Guido's bio.
     guido.add_to_bio('1956: Born in the Netherlands')
     guido.add_to_bio('1982: Earned M.S. Degree from University of Amsterdam')
     guido.add_to_bio('1986: Wrote a glob() routine')
     guido.add_to_bio('1989: Started writing python')
     guido.add_to_bio('2012: Started working for Dropbox')
 
+    # Create a person named "Kim" for Guido to marry.
     kim_name = {'f': 'Kim', 'last': 'Knapp'}
     kim = person(kim_name)
     guido.change_spouse(kim)
 
+    # Print everything you know about Guido.
     print guido.to_string()
 
